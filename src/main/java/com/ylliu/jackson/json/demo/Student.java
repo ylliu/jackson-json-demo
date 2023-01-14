a1a611faf0a1bf3fb3c1526f10af01ca4c77fac8
ylliu <liuming1908@gmail.com>
2023-01-14 11:39:42 +0800

    ignore unknown properties

--- src/main/java/com/ylliu/jackson/json/demo/Student.java
+++ src/main/java/com/ylliu/jackson/json/demo/Student.java
@@ -1,7 +1,10 @@
 package com.ylliu.jackson.json.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.util.Arrays;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Student {
     private int id;
     private String firstName;
